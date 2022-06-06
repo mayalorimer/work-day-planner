a5ffb216ae522f093543774a07ec2e2d45c1da80
Maya Lorimer <maya.lorimer@yahoo.com>
2022-06-06 13:54:28 -0700

    Working on changing classes

--- assets/script.js
+++ assets/script.js
@@ -8,19 +8,19 @@ console.log(currentTime);
 var time = parseInt(currentTime, 10);
 console.log(time);
 
-var table = $('.table');
+var table = $('table');
 
 for (var i = 9; i < 18; i++ ) {
     // the class of hour.get Id is < currentTime return past, etc.
     //change parent class
     // curenntly printing as undefined
-    if (i > time){
-        console.log(table.children().eq(i));
-        table.children().eq(i).removeClass('past');
-        table.children().eq(i).addClass('future');
+    if (i < time){
+        $('#' + i).removeClass('past');
+        $('#' + i).addClass('future');
     }
     else if (i == currentTime) {
-        console.log("true");
+        $('#' + i).removeClass('past');
+        $('#' + i).addClass('present');
     }
     else {
         // remains the same, no change needed?
@@ -30,6 +30,9 @@ for (var i = 9; i < 18; i++ ) {
     
 }
 
-var timeId = table.children().eq(0).children().eq(0).attr("id"); 
-    table.children('tr').removeClass('past');
-    table.children('tr').addClass('future');
+    
+$('#9').removeClass('past');
+
+    //this works
+//$('.time-block').removeClass('past');
+//$('.time-block').addClass('future');
