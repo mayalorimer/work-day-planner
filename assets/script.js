abdcdd068977113c539279239164b0ca701318f9
Maya Lorimer <maya.lorimer@yahoo.com>
2022-06-06 13:01:08 -0700

    Changed formatting and added HTML

--- assets/script.js
+++ assets/script.js
@@ -2,15 +2,7 @@
 var day = moment().format("dddd, MMMM Do");
 $("#currentDay").text(day);
 
-
-//loop to create time blocks for the work day
-for (var i = 9; i < 6; i++){
-    var timeBlock = $('<section>');
-    timeBlock.addClass('time-block');
-    var row = $('<div>');
-    row.addClass("row");
-    var hour = $("<div>");
-    hour.addClass("hour"); 
+var currentTime = moment().format("H");
+console.log(currentTime);
 
 
-}
