34c33c731192f7de911fc9073abb946efda98e60
Maya Lorimer <maya.lorimer@yahoo.com>
2022-06-06 20:09:22 -0700

    added functions for storage features

--- assets/script.js
+++ assets/script.js
@@ -4,9 +4,7 @@ $("#currentDay").text(day);
 
 //variable for the current time
 var currentTime = moment().format("k");
-console.log(currentTime);
 var time = parseInt(currentTime, 10);
-console.log(time);
 
 var table = $('table');
 
@@ -21,3 +19,56 @@ for (var i = 9; i < 18; i++ ) {
         $('#' + i).addClass('present');
     }
 }
+
+//display current todo items if there are any
+var todo = []; 
+todo = JSON.parse(localStorage.getItem("todoList"));
+if (todo != null){
+    for (var i = 0; i < todo.length; i ++){
+        var storedId = todo[i].timeBlock;
+        console.log(storedId);
+        $("#" + storedId).children().eq(1).text(todo[i].todoItem); 
+    }
+}
+
+
+//function for saving to local storage
+
+var saveBtn = document.querySelector(".saveBtn")
+//onclick button to save the written text to local storage
+$('.saveBtn').click(function(event){
+    var id = $(this).parent().attr("id");
+    var textA = $("tr .9").children().eq(1).children().val();
+    var textarea = $(this).siblings('td').eq(0).children().val();
+    console.log(id);
+    console.log(textarea);
+
+
+    //checks for local stoage
+    var todo = []; 
+    var checkStorage = JSON.parse(localStorage.getItem("todoList"));
+    if (checkStorage) {
+        for (var i = 0; i < checkStorage.length; i++){
+            todo.push(checkStorage[i]);
+        }
+        //adds to local storage
+        localStorage.setItem("todoList", JSON.stringify(todo));
+    }
+
+    //adds the new todo item to the todo list
+    todo[todo.length] = {
+        timeBlock: id,
+        todoItem: textarea
+    }
+    localStorage.setItem("todoList", JSON.stringify(todo));
+
+  //  console.log($('.class').val());
+
+
+ //   var text = $('textarea').siblings("textarea").val();
+ //   var time = $('textarea').siblings("tr").attr("id");
+
+  //  localStorage.setItem(time, JSON.stringify(text)); 
+
+}); 
+
